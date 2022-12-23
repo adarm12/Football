942df65d37667f0edecfd1d249497df0ca3954fe
Shanitkk <Shanitk2020@gmail.com>
2022-12-23 23:44:15 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from "react";
-import axios, {formToJSON} from "axios";
+import axios from "axios";
 import {BrowserRouter, Router, Route, NavLink, Routes} from "react-router-dom";
 import LeaguesHomePage from "./LeaguesHomePage";
 import TeamInformation from "./TeamInformation";
@@ -11,11 +11,14 @@ class App extends React.Component {
     state = {
         domain: 'https://app.seker.live/fm1/',
         LeaguesList: [],
+        teamList: [],
         team: 1,
+        leagueName: "spanish",
+        description: "",
     }
 
     componentDidMount() {
-        this.getLeagues();
+        this.getTeam();
     }
 
     getLeagues = () => {
@@ -23,6 +26,14 @@ class App extends React.Component {
             .then((response) => {
                 this.setState(this.state.LeaguesList = response.data)
             });
+    }
+
+    getTeam = () => {
+        axios.get(this.state.domain + '/teams/' + this.state.team)
+            .then((response) => {
+                this.setState(this.state.teamList = response.data)
+                this.setState(this.state.description = "Those are the groups in the " + this.state.leagueName + " league:")
+            })
     }
 
     chooseTeam = (league) => {
@@ -36,14 +47,18 @@ class App extends React.Component {
                 <div className="Title">
                     <label> Football </label>
                 </div>
+                <div>
+                    {this.state.description}
+                </div>
                 <table>
-                    {this.state.LeaguesList.map((league, index) => {
+                    {this.state.teamList.map((league, index) => {
                         return (
                             <tr>
-                                <TeamInformation
-                                    id={league.id}
-                                    name={league.name}
-                                />
+                                {league.name}
+                                {/*<TeamInformation*/}
+                                {/*    id={league.id}*/}
+                                {/*    name={league.name}*/}
+                                {/*/>*/}
                             </tr>
                         )
                     })
