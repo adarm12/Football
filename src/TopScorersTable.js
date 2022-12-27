67abdaf732c79722195963e02983301eb40e7a06
Yotam Efrati <yotamefrati05@gmail.com>
2022-12-27 14:22:37 +0200

    1

--- src/TopScorersTable.js
+++ src/TopScorersTable.js
@@ -1,12 +1,69 @@
 import React from "react";
+import axios from "axios";
 
-function TopScorersTable() {
-    return (
-        <div>
-            Top scorers table
-        </div>
+class TopScorersTable extends React.Component{
 
-    );
+    state = {
+        domain: 'https://app.seker.live/fm1/',
+        leaguesList: [],
+        teamList: [],
+        top3Scorers: [],
+        top3ScorersForProcess: [],
+
+    }
+
+    componentDidMount() {
+        this.getLeagues();
+    }
+
+    getLeagues = () => {
+        axios.get(this.state.domain + 'leagues')
+            .then((response) => {
+                this.setState({
+                    leaguesList: response.data
+                })
+            });
+    }
+
+    getScorers = (leagueId, leagueName) => {
+
+        debugger
+        axios.get(this.state.domain + '/teams/' + leagueId)
+            .then((response) =>  {
+                this.state.teamList = response.data
+            })
+        this.setState({
+            leagueList: []
+        })
+        this.getTeam(this.state.teamList[0])
+    }
+
+    getTeam = (team) => {
+        alert(team.name)
+    }
+
+    getTopThreeFromTeam = () => {
+
+    }
+
+    render() {
+        return (
+            <div className={"main"}>
+                <div>
+                    Top scorers table
+                </div>
+
+                {this.state.leaguesList.map((league,index) => {
+                    return(
+                        <div onClick={() => this.getScorers(league.id,league.name)}>{league.name}</div>
+                    )
+                })}
+
+
+            </div>
+        );
+    }
+
 }
 
 
