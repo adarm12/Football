b2e92e156e6acc463d8793a4616e8b1561f73af8
adarm12 <adarm1234@gmail.com>
2022-12-28 12:17:06 +0200

    .

--- src/LeaguesHomePage.js
+++ src/LeaguesHomePage.js
@@ -40,6 +40,7 @@ class LeaguesHomePage extends React.Component {
 
     }
 
+
     render() {
         return (
             <div className="Main">
--- src/PrintLeaguesTable.js
+++ src/PrintLeaguesTable.js
@@ -1,22 +1,25 @@
 import React from "react";
 
-function PrintLeaguesTable(props)
-{
-    return(
+function PrintLeaguesTable(props) {
+    return (
         <div>
             <table>
                 {props.leagues.map((league, index) => {
                     return (
                         <tr onClick={() => props.chooseTeam(league.id, league.name)}>
+
+                        {/*// <tr onClick={() => props.choosePlayer(2)}>*/}
                             <td>
                                 {league.name}
                             </td>
                         </tr>
                     )
+
                 })
                 }
             </table>
         </div>
     )
 }
+
 export default PrintLeaguesTable;
