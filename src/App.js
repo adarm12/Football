8cb2cab7f720905fc6abf099db0ddcf018a7fb75
Shanitkk <Shanitk2020@gmail.com>
2022-12-31 19:05:46 +0200

    .

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import {BrowserRouter, Router, Route, NavLink, Routes} from "react-router-dom";
 import LeaguesHomePage from "./LeaguesHomePage";
 import GeneralStatisticsPage from "./GeneralStatistics";
-import LeagueResultsHistoryPage from "./LeagueResultsHistory";
+import LeagueResultsHistoryPage from "./LeagueResultsHistoryPage";
 import TopScorersTablePage from "./TopScorersTable";
 import PrintLeaguesTable from "./PrintLeaguesTable";
 import leaguesHomePage from "./LeaguesHomePage";
@@ -37,14 +37,14 @@ class App extends React.Component {
                 </div>
                 <BrowserRouter>
                     <NavLink style={{margin: "30px"}} to={"/LeaguesHomePage"}>Home Page</NavLink>
+                    <NavLink style={{margin: "30px"}} to={"/LeagueResultsHistoryPage"}>League Results History</NavLink>
                     <NavLink style={{margin: "30px"}} to={"/GeneralStatistics"}>General Statistics</NavLink>
-                    <NavLink style={{margin: "30px"}} to={"/LeagueResultsHistory"}>League Results History</NavLink>
                     <NavLink style={{margin: "30px"}} to={"/TopScorersTable"}>Top Scorers Table</NavLink>
 
                     <Routes>
                         <Route path={"/LeaguesHomePage"} element={<LeaguesHomePage/>}/>
+                        <Route path={"/LeagueResultsHistoryPage"} element={<LeagueResultsHistoryPage/>}/>
                         <Route path={"/GeneralStatistics"} element={<GeneralStatisticsPage/>}/>
-                        <Route path={"/LeagueResultsHistory"} element={<LeagueResultsHistoryPage/>}/>
                         <Route path={"/TopScorersTable"} element={<TopScorersTablePage/>}/>
                         <Route path={"*"} element={<LeaguesHomePage/>}/>
                         <Route path={"/"} element={<LeaguesHomePage/>}/>
---
+++ src/LeagueResultsHistoryPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PrintLeaguesTable from "./PrintLeaguesTable";
+
+class LeagueResultsHistoryPage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                League results history
+
+
+            </div>
+        )
+    }
+}
+
+export default LeagueResultsHistoryPage;
